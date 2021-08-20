bd5c8685e7e2bc08f756ab4e5aec6833e4cbe623
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2021-08-20 16:21:42 +0530

    Ex6: Initial Commit

---
+++ Ex6/p-dec.asm
@@ -0,0 +1,39 @@
+; 6b: Sorting in descending order
+
+assume cs: code, ds: data
+data segment
+    count db 03H
+    arr db 75H, 33H, 01H, 25H
+data ends
+code segment
+start:  mov ax, data
+        mov ds, ax
+
+        mov ch, 00H
+        mov cl, count
+
+outer:  mov dx, cx
+        mov si, offset arr
+        mov di, offset arr + 1
+
+inner:  mov al, [si]
+        cmp al, [di]
+
+        ; si > di => CF = 0, already in place
+        jnc skip
+
+        xchg al, [di]
+        mov [si], al
+
+skip:   inc si
+        inc di
+
+        dec dx
+        jnz inner
+
+        loop outer
+
+        mov ah, 4cH
+        int 21H
+code ends
+end start
